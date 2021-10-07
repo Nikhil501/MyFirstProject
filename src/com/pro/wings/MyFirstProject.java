524c25511c500759339cc04500507f540e2a90cf
Nikhil501 <patilnikhil501@gmail.com>
2021-10-07 11:30:06 +0530

    my second commit

--- src/com/pro/wings/MyFirstProject.java
+++ src/com/pro/wings/MyFirstProject.java
@@ -1,8 +1,11 @@
 package com.pro.wings;
 
-public class MyFirstProject {
-    public static void main(String[] args) {
+public class MyFirstProject
+{
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to Java");
+        System.out.println("Change has been done");
     }
 
 }
